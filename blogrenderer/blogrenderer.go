24fe2041d94fa9818c6f61c581ed2d8b92523a00
Thomas Haaland <thomas.haaland@gmail.com>
2023-01-31 12:48:11 +0100

    First commit

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,30 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
