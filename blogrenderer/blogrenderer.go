11b0134066a74fdfa2d5ed2893836bb607c64682
Thomas Haaland <thomas.haaland@gmail.com>
2023-01-31 15:53:45 +0100

    Managed a title

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 var (
@@ -16,13 +17,40 @@ type Post struct {
 	Tags                     []string
 }
 
-func Render(w io.Writer, p Post) error {
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{.Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
 	if err != nil {
 		return err
 	}
 
-	if err := templ.Execute(w, p); err != nil {
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
--- blogrenderer/go.mod
+++ blogrenderer/go.mod
@@ -2,4 +2,4 @@ module github.com/thomashaaland/blogrenderer
 
 go 1.19
 
-require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd // indirect
+require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
